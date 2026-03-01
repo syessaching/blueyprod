8ff6dfd6effb3dff3a1f65912ac6ecc17285d7d6
Chezza Bomediano <syessa.ching@gmail.com>
2026-02-28 19:40:01 -1000

    change cors.rb for origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,13 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001", "127.0.0.1:3001"
-    
-    resource "*",
+    origins 'https://blueyprod.vercel.app'   # <-- your frontend origin
+
+    resource '/cards',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :options],
+      expose: [],
+      max_age: 600,
+      credentials: false
   end
 end
