295bf085760f23708251b1625953491f57357453
Chezza Bomediano <syessa.ching@gmail.com>
2026-03-07 02:19:39 -1000

    Remove unused imports

--- blueyprod-frontend/src/components/CassetteLanding.js
+++ blueyprod-frontend/src/components/CassetteLanding.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import Lottie from 'lottie-react'
 import fireworksAnimation from './Confetti.json'
